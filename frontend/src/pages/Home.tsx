734f2f01419cf95ddec2d4e19b14b29848e043f7
kartikeysharma98281 <kartikeysharma.sharma@gmail.com>
2024-07-03 16:43:13 +0530

    link is added for signin button.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -23,12 +23,14 @@ export const Home = () => {
               </Link>
             </div>
             <div>
+              <Link to="/signin">
               <button
                 type="button"
                 className="w-32 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-              >
+                >
                 Sign In
               </button>
+                </Link>
             </div>
           </div>
         </div>
